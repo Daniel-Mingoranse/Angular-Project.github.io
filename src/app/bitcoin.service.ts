cc09f5cdb13f757ea16d0a23ff7a7d8d6b762ce1
Daniel-Mingoranse <50594387+Daniel-Mingoranse@users.noreply.github.com>
2023-05-10 21:13:33 -0300

    4 files modified

--- src/app/bitcoin.service.ts
+++ src/app/bitcoin.service.ts
@@ -1,47 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-interface Response{
-  time:{
-  update: String;
-  }
+interface Response {
+  time: {
+    update: String;
+  };
   disclaimer: string;
-  bpi:{
-    [key in 'USD' | 'BRL']:{
+  bpi: {
+    [key in 'USD' | 'BRL']: {
       rate: string;
       description: string;
       rate_float: number;
-    }
-  }
+    };
+  };
 }
 
-interface PrioceUpdate{
-  timestamp: Date;
+interface PrioceUpdate {
+  updated: Date;
   USD: number;
   BRL: number;
 }
-
 
 @Injectable()
 export class BitcoinService {
   currentPrice: Response;
   lastUpdate: Date;
+  lasprice: number
 
   UpdateList: Array<PrioceUpdate> = [];
 
-  constructor( private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
-  update(){
-    this.http.get<Response>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
-    .subscribe(data => {
-      this.lastUpdate = new Date();
-      this.currentPrice = data;
-      this.UpdateList.push({
-        timestamp: this.lastUpdate,
-        USD: this.currentPrice.bpi.USD.rate_float,
-        BRL: this.currentPrice.bpi.BRL.rate_float,
+  update() {
+   this.http
+      .get<Response>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
+      .subscribe((data) => {
+        this.lastUpdate = new Date();
+        if (this.currentPrice.bpi.USD.rate_float != this.lasprice) {
+          this.currentPrice = data;
+          this.UpdateList.push({
+            updated: this.lastUpdate,
+            USD: this.currentPrice.bpi.USD.rate_float,
+            BRL: this.currentPrice.bpi.BRL.rate_float,
+          });
+          this.lasprice = this.currentPrice.bpi.USD.rate_float
+          
+        }
+       
       });
-    })
   }
-
 }
--- src/app/bitcoin/bitcoin.component.ts
+++ src/app/bitcoin/bitcoin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BitcoinService } from '../bitcoin.service';
 
 @Component({
   selector: 'app-bitcoin',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bitcoin.component.css']
 })
 export class BitcoinComponent implements OnInit {
+  
 
-  constructor() { }
+  
+  constructor(public bitcoinService: BitcoinService){ }
 
   ngOnInit() {
+    this.update(); 
+    setInterval(() =>{
+      let priceUSD = this.getCurrentPriceUSD();
+      let priceBRL = this.getCurrentPriceBRL();
+      let update = this.getUpdate();
+  
+      console.log(update);
+      
+      let tr = document.getElementById('table') as HTMLElement;
+    
+      tr.innerHTML = `
+      <td>${update} | </td>
+        <td>$${priceUSD.rate_float} | </td>
+        <td>$${priceBRL.rate_float}</td>
+      `;
+    }, 50000)
   }
 
+  getCurrentPriceUSD(){
+    return this.bitcoinService.currentPrice.bpi.USD;
+  }
+  getCurrentPriceBRL(){
+    return this.bitcoinService.currentPrice.bpi.BRL;
+  }
+  getUpdate(){
+    return this.bitcoinService.lastUpdate;
+
+  }
+
+  public update() {
+    this.bitcoinService.update();
+  }
+  
+  
+  
 }
+
